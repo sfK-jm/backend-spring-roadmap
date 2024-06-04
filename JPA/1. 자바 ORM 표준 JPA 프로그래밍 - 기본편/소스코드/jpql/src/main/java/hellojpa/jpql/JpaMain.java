d813ea2985774e8dd17aec72eb5eb772dc8b98fb
sfK <sfk.sjm@gmail.com>
2024-06-04 15:18:22 +0900

    조인

--- JPA/1. 자바 ORM 표준 JPA 프로그래밍 - 기본편/소스코드/jpql/src/main/java/hellojpa/jpql/JpaMain.java
+++ JPA/1. 자바 ORM 표준 JPA 프로그래밍 - 기본편/소스코드/jpql/src/main/java/hellojpa/jpql/JpaMain.java
@@ -16,28 +16,26 @@ public class JpaMain {
 
         try {
 
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
 
-            for (int i = 0; i < 100; i++) {
-                Member member = new Member();
-                member.setUsername("member" + i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Member member = new Member();
+            member.setUsername("teamA");
+            member.setAge(10);
+
+            member.setTeam(team);
+
+            em.persist(member);
 
             em.flush();
             em.clear();
 
-            String jpql = "select m from Member m order by m.age desc";
-            List<Member> resultList = em.createQuery(jpql, Member.class)
-                    .setFirstResult(0)
-                    .setMaxResults(100)
+            String query = "select m from Member m , Team  t where m.username = t.name";
+            List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
 
-            System.out.println("resultList.size() = " + resultList.size());
-
-            for (Member m : resultList) {
-                System.out.println("m = " + m);
-            }
+            System.out.println("result.size() = " + result.size());
 
             tx.commit();
         } catch (Exception e) {
