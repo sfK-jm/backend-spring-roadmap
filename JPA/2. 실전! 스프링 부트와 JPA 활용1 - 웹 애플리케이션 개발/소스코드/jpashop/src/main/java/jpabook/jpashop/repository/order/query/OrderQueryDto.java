921a974c9c261b38b56ffd3171e03cd1dd63c7e3
sfK <sfk.sjm@gmail.com>
2024-07-18 16:43:31 +0900

    finish API 개발 고급 - 컬렉션 조회 최적화(add 주문 조회 V6 + 정리)

--- JPA/2. 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/소스코드/jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ JPA/2. 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/소스코드/jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -19,11 +19,23 @@ public class OrderQueryDto {
     private Address address;
     private List<OrderItemQueryDto> orderItems;
 
-    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate,
+                         OrderStatus orderStatus, Address address) {
         this.orderId = orderId;
         this.name = name;
         this.orderDate = orderDate;
         this.orderStatus = orderStatus;
         this.address = address;
+
+    }
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus,
+                         Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
     }
 }
