c5cf548ccbdd643d02623246f7abf50700b0ba8f
sfK <sfk.sjm@gmail.com>
2024-07-28 00:15:46 +0900

    add @Query, 값, DTO 조회하기

--- JPA/4. 실전! 스프링 데이터JPA/data-jpa/src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ JPA/4. 실전! 스프링 데이터JPA/data-jpa/src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.data_jpa.entity.Member;
+import study.data_jpa.entity.Team;
 
 import java.util.List;
 
@@ -16,6 +17,9 @@ public class MemberRepositoryTest {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    TeamRepository teamRepository;
 
     @Test
     public void testMember() {
@@ -65,5 +69,67 @@ public class MemberRepositoryTest {
         List<Member> findMember = memberRepository.findUser("memberA", 20);
 
         Assertions.assertThat(findMember.get(0)).isEqualTo(member);
+    }
+
+    @Test
+    public void findByUsername() {
+        Member member = new Member("memberA", 5);
+        memberRepository.save(member);
+
+        List<Member> findMember = memberRepository.findByUsername("memberA");
+
+        Assertions.assertThat(findMember.get(0)).isEqualTo(member);
+    }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThan() {
+        Member memberA = new Member("memberA", 100);
+        Member memberB = new Member("memberA", 30);
+        Member memberC = new Member("memberA", 20);
+        Member memberD = new Member("memberA", 10);
+
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+        memberRepository.save(memberC);
+        memberRepository.save(memberD);
+
+        List<Member> findMember = memberRepository.findByUsernameAndAgeGreaterThan("memberA", 20);
+
+        for (Member member : findMember) {
+            System.out.println("member = " + member);
+        }
+    }
+
+    @Test
+    public void findUsernameList() {
+        Member member1 = new Member("memberA", 10);
+        Member member2 = new Member("memberB", 20);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<String> MemberList = memberRepository.findUsernameList();
+
+        for (String s : MemberList) {
+            System.out.println(s);
+        }
+    }
+
+    @Test
+    public void findMemberDto() {
+        Team teamA = new Team("TeamA");
+        teamRepository.save(teamA);
+
+        Member memberA = new Member("memberA", 10, teamA);
+        memberRepository.save(memberA);
+
+        Member memberB = new Member("memberB", 20, teamA);
+        memberRepository.save(memberB);
+
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+
+        for (MemberDto dto : memberDto) {
+            System.out.println("dto = " + dto);
+        }
     }
 }
--- JPA/4. 실전! 스프링 데이터JPA/스프링 데이터JPA.md
+++ JPA/4. 실전! 스프링 데이터JPA/스프링 데이터JPA.md
@@ -839,6 +839,45 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 ## @Query, 값, DTO 조회하기
 
+**단순히 값 하나를 조회**
+
+```java
+@Query("select m.username from Member m")
+List<String> findUsernameList();
+```
+
+JPA 값 타입(`@Embedded`)도 이 방식으로 조회할 수 있다.
+
+**DTO로 직접 조회**
+
+```java
+@Query("select new study.data_jpa.repository.MemberDto(m.id, m.username, t.name) " +
+        "from Member m join m.team t")
+List<MemberDto> findMemberDto();
+```
+
+> [!CAUTION]
+> DTO로 직접 조회 하려면 JPA의 `new`명령어를 사용해야 한다. 그리고 다음과 같이 생성자가 맞는 DTO가 필요하다. (JPA와 사용방식이 동일하다.)
+
+```java
+package study.data_jpa.repository;
+
+import lombok.Data;
+
+@Data
+public class MemberDto {
+    private Long id;
+    private String username;
+    private String teamName;
+
+    public MemberDto(Long id, String username, String teamName) {
+        this.id = id;
+        this.username = username;
+        this.teamName = teamName;
+    }
+}
+```
+
 ## 파라미터 바인딩
 
 ## 바인딩
